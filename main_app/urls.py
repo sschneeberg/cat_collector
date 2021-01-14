aa604c574a471ca82165303bc1a1b710659a8787
sschneeberg <simone.m.schneeberg@gmail.com>
2021-01-14 12:56:18 -0500

    add show route, url with paramters

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('cats/', views.cats_index, name='cats_index')
+    path('cats/', views.cats_index, name='cats_index'),
+    path('cats/<int:cat_id>', views.cats_show, name='cats_show'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,8 @@
 from django.shortcuts import render
+from .models import Cat
 
 # Create your views here.
 # NOTE TO SELF THESE ARE CONTROLLERS
-
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-    
-cats = [
-    Cat('Lolo', 'tabby', 'foul little demon', 3),
-    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-    Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
 
 def index(request):
     return render(request, 'index.html')
@@ -23,4 +11,9 @@ def about(request):
     return render(request, 'about.html')
 
 def cats_index(request):
+    cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
+
+def cats_show(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/show.html', {'cat': cat})
