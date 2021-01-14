bce343f6dab3e45290410143c349215c5598bbb4
sschneeberg <simone.m.schneeberg@gmail.com>
2021-01-14 12:21:14 -0500

    make model

--- main_app/views.py
+++ main_app/views.py
@@ -3,8 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 # NOTE TO SELF THESE ARE CONTROLLERS
 
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+    
+cats = [
+    Cat('Lolo', 'tabby', 'foul little demon', 3),
+    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+    Cat('Raven', 'black tripod', '3 legged cat', 4)
+]
+
 def index(request):
     return render(request, 'index.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def cats_index(request):
+    return render(request, 'cats/index.html', {'cats': cats})
